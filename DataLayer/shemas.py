83509fc1319fd8002f366de58926c3984737516c
Nanashi6 <yuevzhenko@gmail.com>
2025-05-02 18:08:00 +0300

    CRUD для брендов

---
+++ DataLayer/shemas.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel
+from typing import Optional
+from DataLayer.enums import RatingEnum
+
+class Brand(BaseModel):
+    name: Optional[str] = None
+    rating: Optional[RatingEnum] = None
+    share: Optional[float] = None
--- app/__init__.py
+++ app/__init__.py
@@ -10,3 +10,7 @@ migrate = Migrate(app, db)
 
 from DataLayer import models
 from app import routes
+
+from app.blueprints.brands import brands_bp
+
+app.register_blueprint(brands_bp)
