a3978b324c0556f235b1d0d3a931b66e9f98c1f1
Nanashi6 <yuevzhenko@gmail.com>
2025-05-17 20:47:44 +0300

    Исправлен микробаг с нехваткой импорта

--- DataLayer/dao.py
+++ DataLayer/dao.py
@@ -1,4 +1,4 @@
-from typing import Generic, List, TypeVar
+from typing import Generic, List, Optional, TypeVar
 from pydantic import BaseModel
 from sqlalchemy import ColumnElement, delete, select, update
 from sqlalchemy.exc import SQLAlchemyError
