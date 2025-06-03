d6d0f6e695c49352dcc11efd90b5a7fa3d45a976
Nanashi6 <yuevzhenko@gmail.com>
2025-06-03 12:34:18 +0300

    РЕализованы полноценные рабочие сортировки для брендов и товаров внутри брендов

--- app/blueprints/editor/auto_placement.py
+++ app/blueprints/editor/auto_placement.py
@@ -1,7 +1,8 @@
+from collections import defaultdict
 import operator
-from typing import Any, Callable, List, Tuple
+from typing import Any, Callable, Dict, List, Tuple
 from DataLayer.dao import ProductDAO, ShelfUnitDAO
-from DataLayer.models import Product, Shelf
+from DataLayer.models import Brand, Product, Shelf
 from .models import planogram_data, rules_data, server_message, shelf_rule
 from .enums import BrandSorting, ProductSorting
 
@@ -11,12 +12,12 @@ def get_product_sort_key_and_reverse(sort_option: ProductSorting):
     для объекта Product на основе опции ProductSorting.
     """
     sort_keys = {
-        ProductSorting.PriceAsc:    (operator.attrgetter('price'), False),
-        ProductSorting.PriceDesc:   (operator.attrgetter('price'), True),
-        ProductSorting.RatingAsc:   (operator.attrgetter('rating'), False),
-        ProductSorting.RatingDesc:  (operator.attrgetter('rating'), True),
-        ProductSorting.NameAsc:     (operator.attrgetter('name'), False),
-        ProductSorting.NameDesc:    (operator.attrgetter('name'), True),
+        ProductSorting.PriceAsc:    (lambda product: product.price, False),
+        ProductSorting.PriceDesc:   (lambda product: product.price, True),
+        ProductSorting.RatingAsc:   (lambda product: product.SKU_rating.value if product.SKU_rating else 0, False),
+        ProductSorting.RatingDesc:  (lambda product: product.SKU_rating.value if product.SKU_rating else 0, True),
+        ProductSorting.NameAsc:     (lambda product: product.name, False),
+        ProductSorting.NameDesc:    (lambda product: product.name, True),
     }
     return sort_keys.get(sort_option, (None, False))
 
@@ -26,10 +27,10 @@ def get_brand_sort_key_and_reverse(sort_option: BrandSorting):
     для объекта Brand на основе опции BrandSorting.
     """
     sort_keys = {
-        BrandSorting.NameAsc:    (lambda product: product.category_brand_placement.brand.name if product.category_brand_placement and product.category_brand_placement.brand else '', False),
-        BrandSorting.NameDesc:   (lambda product: product.category_brand_placement.brand.name if product.category_brand_placement and product.category_brand_placement.brand else '', True),
-        BrandSorting.RatingAsc:  (lambda product: product.category_brand_placement.brand.rating.value if product.category_brand_placement and product.category_brand_placement.brand and product.category_brand_placement.brand.rating else 0, False),
-        BrandSorting.RatingDesc: (lambda product: product.category_brand_placement.brand.rating.value if product.category_brand_placement and product.category_brand_placement.brand and product.category_brand_placement.brand.rating else 0, True),
+        BrandSorting.NameAsc:    (lambda brand: brand.name, False),
+        BrandSorting.NameDesc:   (lambda brand: brand.name, True),
+        BrandSorting.RatingAsc:  (lambda brand: brand.rating.value if brand.rating else 0, False),
+        BrandSorting.RatingDesc: (lambda brand: brand.rating.value if brand.rating else 0, True),
     }
     return sort_keys.get(sort_option, (None, False))
 
@@ -41,18 +42,41 @@ def get_combined_sorting_steps(rules: rules_data) -> List[Tuple[Callable[[Produc
     brand_key_func, brand_reverse_flag = get_brand_sort_key_and_reverse(rules.brand_sort)
     product_key_func, product_reverse_flag = get_product_sort_key_and_reverse(rules.product_sort)
 
-    sorting_steps = []
+    # sorting_steps = []
 
-    if product_key_func:
-        sorting_steps.append((product_key_func, product_reverse_flag))
+    # if product_key_func:
+    #     sorting_steps.append((product_key_func, product_reverse_flag))
     
-    if brand_key_func:
-        sorting_steps.append((brand_key_func, brand_reverse_flag))
+    # if brand_key_func:
+    #     sorting_steps.append((brand_key_func, brand_reverse_flag))
 
-    return sorting_steps
+    return (brand_key_func, brand_reverse_flag), (product_key_func, product_reverse_flag)
 
-def sort_by_categories(): # IDEA мб не надо, т.к. сортировки покатегорийные будут 
-    ...
+def get_total_product_length_on_category(pps):
+    return sum([p.depth + 0.5 for p in pps])
+
+
+def group_and_sort_by_brand(key, reverse, products: List[Product]) -> Dict[Brand, List[Product]]:
+    grouped_products = defaultdict(list)
+
+    for product in products:
+        grouped_products[product.category_brand_placement.brand].append(product)
+    sorted_brands_list = sorted(list(grouped_products.keys()), key=key, reverse=reverse) 
+    return sorted_brands_list, grouped_products 
+
+def product_sort(key, reverse, products : List[Product]) -> List[Product]:
+    return sorted(products, key=key, reverse=reverse)
+
+def category_sort(brand_sorts, product_sorts, products):
+    sorted_brand_list, grouped_products = group_and_sort_by_brand(brand_sorts[0], brand_sorts[1], products)
+
+    sorted_products = []
+    for key in sorted_brand_list:
+        s_p = product_sort(product_sorts[0], product_sorts[1], grouped_products[key])
+        sorted_products.extend(s_p)
+
+    return sorted_products
+
 
 def solve_dp_for_category(products: List[Product], max_weight: float, max_length: int) -> List[Product]: # FIXME переписать/передумать
     # FIXME Учитывать вес
@@ -86,10 +110,9 @@ def solve_dp_for_category(products: List[Product], max_weight: float, max_length
 
     return taken_products
 
-def calculate_auto_placement_for_shelf(shelf : Shelf, shelf_rule : shelf_rule, sorting_steps):
+def calculate_auto_placement_for_shelf(shelf : Shelf, shelf_rule : shelf_rule, brand_sorts, product_sorts):
     shelf_placed_products = []
     for category_rule in shelf_rule.category_rules:
-        print(category_rule.category_name)
         products_for_shelf = ProductDAO.get_many_for_category(
             category_rule.category_name,
             shelf.height,
@@ -102,15 +125,24 @@ def calculate_auto_placement_for_shelf(shelf : Shelf, shelf_rule : shelf_rule, s
         category_length = share * shelf.length / 100
         category_placed_products = solve_dp_for_category(products_for_shelf, shelf.max_weight, category_length) # FIXME вес исходя из уже стоящих продуктов 
                     
-        for key_func, reverse_flag in sorting_steps:
-            category_placed_products.sort(key=key_func, reverse=reverse_flag)
+        flag = True if len(category_placed_products) > 0 else False
+        products_length = get_total_product_length_on_category(category_placed_products)
+        while flag and products_length < category_length:
+            new_products = solve_dp_for_category(products_for_shelf, shelf.max_weight, category_length - products_length)
+            flag = True if len(new_products) > 0 else False
+            category_placed_products.extend(new_products)
+            products_length = get_total_product_length_on_category(category_placed_products)
+
+        # for key_func, reverse_flag in sorting_steps:
+        #     category_placed_products.sort(key=key_func, reverse=reverse_flag)
+        category_placed_products = category_sort(brand_sorts, product_sorts, category_placed_products)
                     
         shelf_placed_products.extend(category_placed_products)
 
     return shelf_placed_products
 
 def calculate_auto_placement(shelf_unit_id : int, rules : rules_data) -> server_message:
-    sorting_steps = get_combined_sorting_steps(rules)
+    brand_sorts, product_sorts = get_combined_sorting_steps(rules)
 
     # Берём стеллаж
     # Перебираем полки в порядке порядка в правилах или наоборот (находим правило для полки)
@@ -128,7 +160,7 @@ def calculate_auto_placement(shelf_unit_id : int, rules : rules_data) -> server_
                     "product_id": p.id,
                     "position": i,
                     "product": p.to_dict()
-                } for i, p in enumerate(calculate_auto_placement_for_shelf(shelf, shelf_rule, sorting_steps))])
+                } for i, p in enumerate(calculate_auto_placement_for_shelf(shelf, shelf_rule, brand_sorts, product_sorts))])
             else:
                 # тут сообнение об ошибке с полкой
                 ...
