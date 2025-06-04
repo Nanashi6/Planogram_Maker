d8b8eac8ab24210de2f09a3cb4712668e25eec4f
Nanashi6 <yuevzhenko@gmail.com>
2025-06-05 01:06:14 +0300

    ВЫполнен перевод иинтерфейса на русский

--- app/blueprints/products/forms.py
+++ app/blueprints/products/forms.py
@@ -1,38 +1,50 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SelectField, SubmitField
-from wtforms.validators import DataRequired, NumberRange
+from wtforms import StringField, IntegerField, SelectField, SubmitField, DecimalField 
+from wtforms.validators import DataRequired, NumberRange, Optional 
 from DataLayer.enums import RatingEnum, SegmentEnum
 from DataLayer.dao import CategoryBrandPlacementDAO
 
 class CreateProduct(FlaskForm):
     segment = SelectField(
-        'Segment',
-        validators=[DataRequired()],
-        choices=[(segment.value, segment.value) for segment in SegmentEnum],
-        coerce=SegmentEnum
+        'Сегмент', 
+        validators=[DataRequired(message="Пожалуйста, выберите сегмент.")],
+        choices=[(segment.value, segment.value) for segment in SegmentEnum], 
+        coerce=SegmentEnum 
     )
-    name = StringField('Product Name', validators=[DataRequired()])
-    barcode = IntegerField('Barcode', validators=[DataRequired()])
+    name = StringField('Название продукта', validators=[DataRequired(message="Пожалуйста, введите название продукта.")]) 
+    barcode = StringField('Штрихкод', validators=[DataRequired()])
     SKU_rating = SelectField(
-        'SKU Rating',
-        validators=[DataRequired()],
-        choices=[(rating.value, rating.value) for rating in RatingEnum],
-        coerce=RatingEnum
+        'Рейтинг SKU', 
+        validators=[DataRequired(message="Пожалуйста, выберите рейтинг SKU.")],
+        choices=[(rating.value, rating.value) for rating in RatingEnum], 
+        coerce=RatingEnum 
     )
-    length = IntegerField('Length', validators=[DataRequired()]) 
-    depth = IntegerField('Depth', validators=[DataRequired()])
-    height = IntegerField('Height', validators=[DataRequired()])
-    weight = IntegerField('Weight', validators=[DataRequired()])
-    price = IntegerField('Price', validators=[DataRequired()])
+
+    length = DecimalField('Длина (см)', validators=[DataRequired(), NumberRange(min=0, message="Длина не может быть отрицательной.")], places=2) 
+    depth = DecimalField('Глубина (см)', validators=[DataRequired(), NumberRange(min=0, message="Глубина не может быть отрицательной.")], places=2) 
+    height = DecimalField('Высота (см)', validators=[DataRequired(), NumberRange(min=0, message="Высота не может быть отрицательной.")], places=2) 
+    weight = DecimalField('Вес (кг)', validators=[DataRequired(), NumberRange(min=0, message="Вес не может быть отрицательным.")], places=3) 
+    price = DecimalField('Цена', validators=[DataRequired(message="Пожалуйста, укажите цену."), NumberRange(min=0, message="Цена не может быть отрицательной.")], places=2) 
+
+    category_brand_placement_id = SelectField(
+        'Связка Категория-Бренд', 
+        validators=[DataRequired(message="Пожалуйста, выберите связку категория-бренд.")],
+        choices=[],
+        coerce=int
+    )
 
     def __init__(self, *args, **kwargs):
         super(CreateProduct, self).__init__(*args, **kwargs)
-        self.category_brand_placement_id.choices = [(item.id, f'{item.category.name} {item.brand.name}') for item in CategoryBrandPlacementDAO.get_all()]
+        self.category_brand_placement_id.choices = [
+            (item.id, f'{item.category.name} - {item.brand.name}')
+            for item in CategoryBrandPlacementDAO.get_all()
+        ]
+        if not self.category_brand_placement_id.choices:
+            self.category_brand_placement_id.choices = [(0, "Нет доступных связок Категория-Бренд")] 
 
-    category_brand_placement_id = SelectField('Category-Brand Placement', validators=[DataRequired()], choices=[])
 
-    submit = SubmitField('Create')
+    submit = SubmitField('Создать') 
 
 class UpdateProduct(CreateProduct):
-    id = IntegerField('ID', validators=[DataRequired()])
-    submit = SubmitField('Update')
+    id = IntegerField('ID', render_kw={'readonly': True})
+    submit = SubmitField('Обновить') 
