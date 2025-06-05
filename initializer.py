5f789da78ece7e181bff7d7da2f32a5f40deb9b3
Nanashi6 <yuevzhenko@gmail.com>
2025-06-05 14:45:49 +0300

    Обновлён инициализатор БД

--- initializer.py
+++ initializer.py
@@ -1,6 +1,6 @@
 import pandas as pd
-from DataLayer.shemas import Category, Brand, Product, ShelfUnit, Shelf, Planogram, PlacedProduct, CategoryBrandPlacement
-from DataLayer.dao import CategoryDAO, BrandDAO, ProductDAO, ShelfUnitDAO, ShelfDAO, PlanogramDAO, PlacedProductDAO, CategoryBrandPlacementDAO
+from DataLayer.shemas import Category, Brand, Product, ShelfUnit, Shelf, Planogram, PlacedProduct
+from DataLayer.dao import CategoryDAO, BrandDAO, ProductDAO, ShelfUnitDAO, ShelfDAO, PlanogramDAO, PlacedProductDAO
 from DataLayer.enums import SegmentEnum, RatingEnum
 
 segments = {
@@ -28,18 +28,16 @@ def init():
         init_categories()
     if not BrandDAO.get_all():
         init_brands()
-    if not CategoryBrandPlacementDAO.get_all():
-        init_catBrPlacements()
     if not ProductDAO.get_all():
         init_products()
     if not ShelfUnitDAO.get_all():
         init_shelfUnits()
     if not ShelfDAO.get_all():
         init_shelves()
-    if not PlanogramDAO.get_all():
-        init_planograms()
-    if not PlacedProductDAO.get_all():
-        init_placedProducts()
+    # if not PlanogramDAO.get_all():
+    #     init_planograms()
+    # if not PlacedProductDAO.get_all():
+    #     init_placedProducts()
     else:
         print("INFO: skipping DB initialization.")
 
@@ -47,7 +45,7 @@ def init():
 
 def init_categories(df = pd.read_excel("C:/Users/Yury Youzhanka/Desktop/DiplomaData/Исходные данные (Автохимия).xlsx", sheet_name='Лист2')):    
     unique_indices = df.drop_duplicates(subset=['Категория'], keep='first').index.to_list()
-    categories = [Category(name=ser['Категория'], share=ser['Доля категории, %']) for ind, ser in df.loc[unique_indices].iterrows()]    
+    categories = [Category(name=ser['Категория']) for ind, ser in df.loc[unique_indices].iterrows()]    
     CategoryDAO.add_many(categories)
 
 def init_brands(df = pd.read_excel("C:/Users/Yury Youzhanka/Desktop/DiplomaData/Исходные данные (Автохимия).xlsx", sheet_name='Лист2')):
@@ -57,15 +55,6 @@ def init_brands(df = pd.read_excel("C:/Users/Yury Youzhanka/Desktop/DiplomaData/
         rating=ratings[str(ser['Рейтинг бренда'])]
     ) for ind, ser in df.loc[unique_indices].iterrows()]
     BrandDAO.add_many(brands)
-
-def init_catBrPlacements(df = pd.read_excel("C:/Users/Yury Youzhanka/Desktop/DiplomaData/Исходные данные (Автохимия).xlsx", sheet_name='Лист2')):
-    unique_indices = df.drop_duplicates(subset=['Бренд', 'Категория'], keep='first').index.to_list()
-    catBrPlacements = [CategoryBrandPlacement(
-        brand_id=BrandDAO.get_one(Brand(name=ser['Бренд'])).id,
-        category_id=CategoryDAO.get_one(Category(name=ser['Категория'])).id,
-        share=ser['Доля бренда, %'] if ser['Доля бренда, %'] != '-' else None
-    ) for ind, ser in df.loc[unique_indices].iterrows()]
-    CategoryBrandPlacementDAO.add_many(catBrPlacements)
 
 def init_products(df = pd.read_excel("C:/Users/Yury Youzhanka/Desktop/DiplomaData/Исходные данные (Автохимия).xlsx", sheet_name='Лист2'), 
                   df2 = pd.read_excel("C:/Users/Yury Youzhanka/Desktop/DiplomaData/Исходные данные (Автохимия).xlsx", sheet_name='Характеристики товаров')):
@@ -79,10 +68,8 @@ def init_products(df = pd.read_excel("C:/Users/Yury Youzhanka/Desktop/DiplomaDat
         height=df2[df2['Штрихкод'] == ser['Штрихкод']]['Высота, см.'],
         weight=df2[df2['Штрихкод'] == ser['Штрихкод']]['Масса, кг.'],
         price=df2[df2['Штрихкод'] == ser['Штрихкод']]['Цена, руб.'],
-        category_brand_placement_id=CategoryBrandPlacementDAO.get_one(CategoryBrandPlacement(
-                brand_id=BrandDAO.get_one(Brand(name=ser['Бренд'])).id,
-                category_id=CategoryDAO.get_one(Category(name=ser['Категория'])).id,
-            )).id,
+        brand_id=BrandDAO.get_one(Brand(name=ser['Бренд'])).id,
+        category_id=CategoryDAO.get_one(Category(name=ser['Категория'])).id
     ) for ind, ser in df.iterrows()]
     ProductDAO.add_many(products)
 
@@ -101,16 +88,16 @@ def init_shelves(df = pd.read_excel("C:/Users/Yury Youzhanka/Desktop/DiplomaData
                         ) for ind, ser in df.iterrows()]
     ShelfDAO.add_many(shelves)
 
-def init_planograms():
-    planogram = Planogram(name='test', shelf_unit_id=ShelfUnitDAO.get_all().pop().id)
-    PlanogramDAO.add(planogram)
+# def init_planograms():
+#     planogram = Planogram(name='test', shelf_unit_id=ShelfUnitDAO.get_all().pop().id)
+#     PlanogramDAO.add(planogram)
 
-def init_placedProducts(df = pd.read_excel("C:/Users/Yury Youzhanka/Desktop/DiplomaData/Автохимия апрель 2024.xlsx", sheet_name='Вертикально')):
-    placed_products = []
-    for i in range(1,5):
-        for ind, row in df[i].dropna().items():
-            placed_products.append(PlacedProduct(shelf_id=ShelfDAO.get_one(Shelf(shelf_number=i)).id,
-                                                     product_id=ProductDAO.get_one(Product(name=row)).id,
-                                                     planogram_id=PlanogramDAO.get_all()[0].id,
-                                                     position=ind))
-    PlacedProductDAO.add_many(placed_products)
+# def init_placedProducts(df = pd.read_excel("C:/Users/Yury Youzhanka/Desktop/DiplomaData/Автохимия апрель 2024.xlsx", sheet_name='Вертикально')):
+#     placed_products = []
+#     for i in range(1,5):
+#         for ind, row in df[i].dropna().items():
+#             placed_products.append(PlacedProduct(shelf_id=ShelfDAO.get_one(Shelf(shelf_number=i)).id,
+#                                                      product_id=ProductDAO.get_one(Product(name=row)).id,
+#                                                      planogram_id=PlanogramDAO.get_all()[0].id,
+#                                                      position=ind))
+#     PlacedProductDAO.add_many(placed_products)
